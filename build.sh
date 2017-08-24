92a7653deef0d2795f7f0dec7e757d5903b1238a
root <root@localhost.localdomain>
2017-08-24 11:44:22 -0400

    Adding helpful build script

---
+++ build.sh
@@ -0,0 +1,24 @@
+export PATH=$PATH:$HOME/miniconda2/bin
+echo Clearing out conda-build
+rm -rf ~/miniconda2/conda-bld
+mkdir -p ~/miniconda2/conda-bld/linux-64
+mkdir -p ~/miniconda2/conda-bld/noarch
+conda index ~/miniconda2/conda-bld/linux-64
+conda index ~/miniconda2/conda-bld/noarch
+echo Ordering channels
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
+conda config --add channels local
+cd recipes
+recipeFolders=$(ls)
+for f in $recipeFolders; do
+  echo "Starting build for $f"
+  conda build $f --old-build-string -q
+done
+cd ..
+cp -a ~/miniconda2/conda-bld/linux-64 .
+echo
+echo
+echo
+cd linux-64 && ls
