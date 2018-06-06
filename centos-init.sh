6d10228329e06a1799e3889e47d8800806f7ef42
wkhunt <wkhunt3@gmail.com>
2018-06-05 23:04:35 -0400

    Added scipy with np 114

--- centos-init.sh
+++ centos-init.sh
@@ -1,3 +1,2 @@
 yum -y install epel-release && yum -y update
-yum -y install zlib-devel wget gcc gcc-c++ autogen libtool make autoconf automake freetype-devel SDL-devel bzip2 swig openssl-devel
-
+yum -y install zlib-devel wget gcc gcc-c++ autogen libtool make autoconf automake freetype-devel SDL-devel bzip2 swig openssl-devel devtoolset-2-gcc-gfortran
--- ubuntu-init.sh
+++ ubuntu-init.sh
@@ -1,1 +1,1 @@
-sudo apt-get install libtool autogen make automake swig bzip2 zlib1g-dev libsdl1.2-dev libfreetype6 libfreetype6-dev
+sudo apt-get install libtool autogen make automake swig bzip2 zlib1g-dev libsdl1.2-dev libfreetype6 libfreetype6-dev gfortran
