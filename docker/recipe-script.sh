ffa9094e05df27e593209221c291ca6c6ebb0de0
Patrick D <patrick.doody@rgi-corp.com>
2018-07-05 13:38:34 -0400

    Simplify docker script logic

--- docker/recipe-script.sh
+++ docker/recipe-script.sh
@@ -2,13 +2,13 @@ export PATH="$HOME/miniconda2/bin:$PATH"
 echo Clearing out conda-bld
 rm -rf ~/miniconda2/conda-bld
 
-echo Rebuilding conda-bld
+echo "Rebuilding conda-bld"
 mkdir -p ~/miniconda2/conda-bld/linux-64
 mkdir -p ~/miniconda2/conda-bld/noarch
 conda index ~/miniconda2/conda-bld/linux-64
 conda index ~/miniconda2/conda-bld/noarch
 
-echo Adding channels
+echo "Adding channels"
 conda config --remove channels defaults
 conda config --add channels defaults
 conda config --add channels conda-forge
@@ -16,10 +16,18 @@ conda config --add channels bioconda
 conda config --add channels local
 
 # Install all of the dependencies required for the recipe builds
+echo "Downloading public prerequisites"
 cd share
 conda env create -f environment.yml
+conda index ~/miniconda2/pkgs
+
+# Remove the public channels so ensure that no versions that we do not specify are accidentally pulled in
+conda config --remove channels conda-forge
+conda config --remove channels bioconda
+conda config --remove channels defaults
 
 # Loop through each Recipe folder and build it
+echo "Building recipes"
 cd share/recipes
 vendoredFolders=$(ls)
 for f in $vendoredFolders; do
@@ -28,26 +36,10 @@ for f in $vendoredFolders; do
 done
 cd
 
-# Do not delete the recipe artifacts that we have built or have declared. By setting this flag here, they will not be deleted during cleanup.
-toKeep=$(ls ~/miniconda2/conda-bld/linux-64)
-for f in $toKeep; do
-  echo "Keeping $f"
-done
-
 # Update the Conda index to the specified linux-64 directory
 mkdir linux-64 && cd linux-64
-wget -q -r -l1 -e robots=off -nH -nd --reject="index.html*" --no-parent --no-cookies SED_CHANNEL/linux-64/ --user=SED_USER --password=SED_PASSWORD
 mv ~/miniconda2/conda-bld/linux-64/* .
-conda index .
-
-# Cleanup; delete all artifacts that are not flagged to be kept. 
-deleteString="find . -type f"
-for f in $toKeep; do
-  deleteString=$deleteString" ! -name $f"
-done
-
-deleteString=$deleteString" -delete"
-$deleteString
+mv ~/miniconda2/pkgs/*.tar.bz2 .
 
 # Anything remaining in the Share will be pushed to Nexus in the JenkinsFile
 cd ..
